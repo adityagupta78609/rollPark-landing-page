29a4145d564cb0a3276802a9aef5f6cfe83d3def
adityagupta78609 <92269084+adityagupta78609@users.noreply.github.com>
2023-03-30 03:14:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import './App.css';
 import { useState,useEffect } from 'react';
-// import Navbar from './components/Navbar';
-// import Pagination from './components/Pagination';
-// import Cards from './components/Cards';
-// import Section from './components/Section';
-// import RightSection from './components/RightSection';
-// import Animation from './components/Animation';
 import NavbarRoll from './components/NavbarRoll';
 import VideoComp from './components/VideoComp';
 import SchemeStrip from './components/SchemeStrip';
@@ -23,45 +17,16 @@ import Footer from './components/Footer';
 import NavbarTail from './components/NavbarTail';
 
 
-//Used dummy api
-// const api = "https://jsonplaceholder.typicode.com/photos";
+
 
 
 
 function App() {
   
-  // kept loggedIn variable true to show functionalities of add and delete images buttons
-
-  /*
-  const [loggedIn,setLoggedIn]= useState(true);
-  const [key,setKey] = useState("");
-
-  const searchtext = (text) =>{
-        setKey(text);
-  }
-
-
-*/      
+ 
   return (<div
     >
-      {/*   style={{ */}
-      {/*  "overflow":"hidden"}} */}
-    {/* <div className="App">
-     
-     <Navbar func = {searchtext}></Navbar>
-     <div className="main flex flex-wrap ">
       
-        {loggedIn == true && <RightSection/>}
-<Pagination API = "https://jsonplaceholder.typicode.com/photos" numberOfPostsPerPage={9}/>
-      
-
-
-     </div>
-     
-    </div> */}
-
-    {/* <Animation></Animation>
-     */}
      
      
     <div className='bg-white'>
@@ -87,7 +52,7 @@ function App() {
      
       <Footer/>
 
-{/* <NavbarTail/> */}
+
      
       </div>
   );
